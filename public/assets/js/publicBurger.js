3f749204e04715fc602c0459d81c1533640dc55d
Joy Chen <joychen5069@gmail.com>
2020-05-14 20:15:04 -0400

    got the add function to work

--- public/assets/js/publicBurger.js
+++ public/assets/js/publicBurger.js
@@ -29,7 +29,7 @@ $(function() {
   
       var newBurger = {
         name: $("#ca").val().trim(),
-        devoured: $("[devoured=true]:checked").val().trim()
+        // devoured: $("[devoured=true]:checked").val().trim()
       };
   
       // Send the POST request.
