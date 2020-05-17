22dd678e4cca9ddc72d77c33c58b15d5c85acc26
Joy Chen <joychen5069@gmail.com>
2020-05-17 14:34:45 -0400

    debugging heroku

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,16 @@
 const mysql = require("mysql");
 
-if(process.send.JAWSDB_URL) {
-  connetion = mysql.createConnection(process.env.JAWSDB_URL)
+const connection;
+if (process.send.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
-
-connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "joychen5069",
-  database: "burgers_db"
-})
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "joychen5069",
+    database: "burgers_db"
+  })
 };
 
 connection.connect((err) => {
